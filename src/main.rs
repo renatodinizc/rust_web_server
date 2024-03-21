fbd819b5015ee016d7cf1085f19f33917d80c920
Renato Diniz <renatodiniz25@gmail.com>
2024-03-21 13:10:11 -0300

    refactor: refact code

--- src/main.rs
+++ src/main.rs
@@ -3,35 +3,44 @@ use std::{
     io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
     thread,
-    time::Duration,
 };
 
-fn main() {
-    let listener = TcpListener::bind("localhost:8080").unwrap();
+fn main() -> Result<(), std::io::Error> {
+    let listener = TcpListener::bind("localhost:8080").expect("Could not bind to specified port");
 
-    for stream in listener.incoming() {
-        let stream = stream.unwrap();
-
-        thread::spawn(move || {
-            handle_connection(stream);
+    listener
+        .incoming()
+        .filter_map(|stream| match stream {
+            Err(e) => {
+                eprintln!("Couldn't get the connection from stream: {e}");
+                None
+            }
+            Ok(stream) => Some(stream),
+        })
+        .for_each(|stream| {
+            thread::spawn(move || {
+                handle_connection(stream);
+            });
         });
-    }
+
+    Ok(())
 }
 
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
-    let request_line = buf_reader.lines().next().unwrap().unwrap();
+    let request_line = buf_reader
+        .lines()
+        .next()
+        .unwrap()
+        .expect("Failed to read request stream");
 
     let (status_line, filename) = match &request_line[..] {
-        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
-        "GET /sleep HTTP/1.1" => {
-            thread::sleep(Duration::from_secs(5));
-            ("HTTP/1.1 200 OK", "hello.html")
-        }
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "index.html"),
+        "GET /hello_world HTTP/1.1" => ("HTTP/1.1 200 OK", "hello_world.html"),
         _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
     };
 
-    let contents = fs::read_to_string(filename).unwrap();
+    let contents = fs::read_to_string(format!("www/{filename}")).expect("Couldn't parse file");
     let length = contents.len();
     let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
 
